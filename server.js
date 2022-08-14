c8630200f0e1fcac896e6be6e45041d9aa725f5d
quyentm <khunglongditoongasdasd@gmail.com>
2022-08-14 15:30:07 +0700

    update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/benhvien-fe'));
+app.use(express.static('./src'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/benhvien-fe/'}),
+    res.sendFile('index.html', {root: 'src/'}),
 );
 
 // Start the app by listening on the default Heroku port
