cd52963c044afc4c8bbc5e2a7bcf4fda553547ba
quyentm <khunglongditoongasdasd@gmail.com>
2022-08-14 14:59:12 +0700

    update

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
